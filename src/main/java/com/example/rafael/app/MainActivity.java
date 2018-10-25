46a4de9b2f6e4f2b6f3227e564e42a1e278fedeb
EdgardoAvilaPerez <42726927+EdgardoAvilaPerez@users.noreply.github.com>
2018-10-25 11:27:57 -0600

    Add files via upload

---
+++ src/main/java/com/example/rafael/app/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.rafael.app;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager viewPager;
+
+    EditText t1;
+    TextView lblMostar;
+    private int mYearIni, mMonthIni, mDayIni, sYearIni, sMonthIni, sDayIni;
+    static final int DATE_ID = 0;
+    Calendar C = Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sMonthIni = C.get(Calendar.MONTH);
+        sDayIni = C.get(Calendar.DAY_OF_MONTH);
+        sYearIni = C.get(Calendar.YEAR);
+        this.t1 = findViewById(R.id.txtFecha);
+        this.lblMostar = findViewById(R.id.lblMostar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyTimerTask(),500,4000);
+
+
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showDialog(DATE_ID);
+            }
+        });
+    }
+
+    public  class MyTimerTask extends TimerTask{
+
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(viewPager.getCurrentItem() == 0 ){
+                        viewPager.setCurrentItem(1);
+                    }else if(viewPager.getCurrentItem() == 1){
+                        viewPager.setCurrentItem(2);
+                    }else{
+                        viewPager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
+    }
+
+
+    private void colocar_fecha() {
+        t1.setText("'"+mDayIni + "-" + (mMonthIni + 1) + "-" + mYearIni+"'");
+
+    }
+
+
+
+    private DatePickerDialog.OnDateSetListener mDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYearIni = year;
+                    mMonthIni = monthOfYear;
+                    mDayIni = dayOfMonth;
+                    colocar_fecha();
+
+                }
+
+            };
+
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_ID:
+                return new DatePickerDialog(this, mDateSetListener, sYearIni, sMonthIni, sDayIni);
+        }
+
+
+        return null;
+    }
+    /*
+    public Date sumarDias(Date fecha,int dias){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(fecha);
+        calendar.add(Calendar.MONTH,dias);
+        return calendar.getTime();
+    }
+    */
+}
